f0021bb13f485034350931ecdce2000f936ce8b0
Alexey <charushkin.lexa@yandex.ru>
2023-03-29 09:48:36 +0300

    feat: start

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"ru.yandex.practicum.filmorate"})
+//@ComponentScan({"ru.yandex.practicum.filmorate"})
 public class FilmorateApplication {
     public static void main(String[] args) {
         SpringApplication.run(FilmorateApplication.class, args);
